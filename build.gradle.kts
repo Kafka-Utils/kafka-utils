51996797929ecd66eac5b63cbe4f8d7b78ee896b
Henrique Mota <henriquebrasileiro@zgsolucoes.com.br>
2021-04-17 18:24:27 -0300

    [KU-6] initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.4.30"
+    id("org.jetbrains.kotlin.kapt") version "1.4.30"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.30"
+    id("groovy")
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("io.micronaut.application") version "1.4.2"
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("codenarc")
+}
+
+version = "0.1"
+group = "br.com.kafkautils"
+
+val kotlinVersion = project.properties.get("kotlinVersion")
+repositories {
+    mavenCentral()
+}
+
+micronaut {
+    runtime("netty")
+    testRuntime("spock2")
+    processing {
+        incremental(true)
+        annotations("br.com.kafkautils.*")
+    }
+}
+
+dependencies {
+    kapt("io.micronaut.data:micronaut-data-processor")
+    kapt("io.micronaut.openapi:micronaut-openapi")
+    kapt("io.micronaut.security:micronaut-security-annotations")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut:micronaut-multitenancy")
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut.beanvalidation:micronaut-hibernate-validator")
+    implementation("io.micronaut.data:micronaut-data-jdbc")
+    implementation("io.micronaut.flyway:micronaut-flyway")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.security:micronaut-security")
+    implementation("io.micronaut.security:micronaut-security-jwt")
+    implementation("io.micronaut.sql:micronaut-jdbc-hikari")
+    implementation("io.swagger.core.v3:swagger-annotations")
+    implementation("javax.annotation:javax.annotation-api")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:spock")
+    testImplementation("org.testcontainers:testcontainers")
+    compileOnly("org.graalvm.nativeimage:svm")
+    implementation("io.micronaut:micronaut-validation")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-extension-functions")
+    implementation("io.micronaut.rxjava3:micronaut-rxjava3")
+    implementation("io.micronaut.cache:micronaut-cache-caffeine")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+    runtimeOnly("org.postgresql:postgresql")
+}
+
+extensions.getByType(CodeNarcExtension::class.java).toolVersion = "2.1.0"
+tasks.withType(CodeNarc::class.java) {
+    group = "verification"
+}
+
+application {
+    mainClass.set("br.com.kafkautils.ApplicationKt")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("11")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+}
---
+++ src/main/kotlin/br/com/kafkautils/Application.kt
@@ -0,0 +1,20 @@
+package br.com.kafkautils
+
+import io.micronaut.runtime.Micronaut.build
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+
+@OpenAPIDefinition(
+    info = Info(
+        title = "kafka-utils",
+        version = "0.0"
+    )
+)
+object Api
+
+fun main(args: Array<String>) {
+    build()
+        .args(*args)
+        .packages("br.com.kafkautils")
+        .start()
+}
